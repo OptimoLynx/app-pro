8c58cef29071a7eacff036287eaa4db9fd5bc434
OptimoLynx <optimolynx@gmail.com>
2023-12-12 10:02:01 +0100

    pure jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const nam = "JD";
+const num = 7;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const fName = "Amy";
+const lName = "Flor";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Food</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+
+    <h2>Hi {nam}!</h2>
+    <p>Your lucky number is {num}</p>
+    <p>Your unlucky number is {num + 4}</p>
+
+    <h2>Hello {fName} {lName}!</h2>
+    <h3>Hello {`${nam} ${lName}`}!</h3>
+  </div>, document.getElementById("root"));
