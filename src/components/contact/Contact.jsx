464c086a4fe2efd9f0e354a544d0c519b070bb18
OptimoLynx <optimolynx@gmail.com>
2023-12-12 17:37:28 +0100

    Props practice

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,46 +1,27 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../../contact";
 
-const data = [
-  {
-    name: "Selena",
-    img: "https://purepng.com/public/uploads/large/51506280614zb1n2yuuih10rdcgf1a3kd9qtjwhtwoatreqs9uwfth5iwuo4hobi4x2wp8jqrhaqw0r30nliagtcp8dnbkna6jmhkhldx3cc8kn.png",
-    alt: "avatar_img",
-    tel: "+123 456 789",
-    email: "sg@sogmez.com",
-  },
-  {
-    name: "Justin",
-    img: "https://purepng.com/public/uploads/large/a-famous-singer-justin-bieber-pqc.png",
-    alt: "avatar_img",
-    tel: "+123 456 789",
-    email: "jg@jbieb.com",
-  },
-  {
-    name: "Ariana",
-    img: "https://purepng.com/public/uploads/large/a-famous-singer-ariana-grande-vmw.png",
-    alt: "avatar_img",
-    tel: "+123 456 789",
-    email: "r@arians.com",
-  },
-];
-
-const listItems = data.map((e) => {
+function createCard(contact) {
   return (
-    <>
-      <div className="card">
-        <h2>{e.name}</h2>
-        <img src={e.img} alt={e.alt} />
-        <p>{e.tel}</p>
-        <p>{e.email}</p>
-      </div>
-    </>
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.img}
+      tel={contact.tel}
+      email={contact.email}
+    />
   );
-});
+}
 
 function Contact(props) {
   return (
     <>
-      <div>{listItems}</div>
+      <div>
+        <h1 className="heading">Fan Favorite Singers</h1>
+        {contacts.map(createCard)}
+      </div>
     </>
   );
 }
