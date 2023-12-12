4c1ef039e751443d9be6aff3a0107e0c12b19a1a
OptimoLynx <optimolynx@gmail.com>
2023-12-12 11:56:08 +0100

    import export

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Heading from "./Heading";
 import Luck from "./Luck";
 import Name from "./Name";
+import Math from "./Math";
 
 function App() {
   return (
     <div className="flex-container">
       <Heading />
+      <Math />
       <Luck />
       <Name />
     </div>
---
+++ src/components/Math.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as pi from "../math";
+
+function Math() {
+  return (
+    <ul>
+      <li>{pi.default}</li>
+      <li>{pi.doublePi()}</li>
+      <li>{pi.triplePi()}</li>
+    </ul>
+  );
+}
+
+export default Math;
