da44c5f2dd95f7e63411345c77d2afbd18ac838c
OptimoLynx <optimolynx@gmail.com>
2023-12-12 20:54:58 +0100

    Arrow Functions

--- src/components/emoji/Emoj.jsx
+++ src/components/emoji/Emoj.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import EmoCard from "./EmoCard";
 import emos from "../../emo";
 
-function createEmoji(emo) {
-  return (
-    <EmoCard
-      id={emo.id}
-      key={emo.id}
-      emoji={emo.emoji}
-      name={emo.name}
-      meaning={emo.meaning.substring(0, 100)}
-    />
-  );
-}
-
 function Emoji() {
   return (
     <div>
       <h1 className="emo">
         <span>emopedia</span>
       </h1>
-      <dl className="dictionary">{emos.map(createEmoji)}</dl>
+      <dl className="dictionary">
+        {emos.map(function createEmoji(emo) {
+          return (
+            <EmoCard
+              id={emo.id}
+              key={emo.id}
+              emoji={emo.emoji}
+              name={emo.name}
+              meaning={emo.meaning.substring(0, 100)}
+            />
+          );
+        })}
+      </dl>
     </div>
   );
 }
--- src/components/keeper/Note.jsx
+++ src/components/keeper/Note.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import NoteCard from "./NoteCard";
 import notes from "../../notes";
 
-function createNotes(noteItem) {
-  return (
-    <NoteCard
-      key={noteItem.key}
-      title={noteItem.title}
-      content={noteItem.content}
-    />
-  );
-}
-
 function Note() {
-  return <div>{notes.map(createNotes)}</div>;
+  return (
+    <div>
+      {notes.map((noteItem) => (
+        <NoteCard
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Note;
