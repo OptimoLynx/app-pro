7b7eae52d4d9cfa70e4831fc3f1afa993915d42d
Nhkum, Htoi <htoi.nhkum@studiosus.com>
2023-12-15 15:47:46 +0100

    use state for dynamic time change

--- src/components/hook/General.jsx
+++ src/components/hook/General.jsx
@@ -20,8 +20,7 @@ function General() {
   return (
     <div className="container">
       <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <button onClick={increase}>Get Time</button>
     </div>
   );
 }
